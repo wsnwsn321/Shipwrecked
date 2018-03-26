804cec36cdca71d0a5e9fbd28f1cc71a575589fb
Drake Addis <>
2018-03-26 16:11:15 -0400

    additional adjustments to gun stuff

--- Assets/Scripts/Gun Scripts/NewGun.cs
+++ Assets/Scripts/Gun Scripts/NewGun.cs
@@ -39,8 +39,26 @@ public class NewGun : PlayerManager {
 
 			//this if checks if the player has remaining ammo
 		if (!gameObject.GetComponentInParent<CoreControl> ().IsReloading ()) {
-			if (gameObject.GetComponentInParent<AmmoRemaining> ().ammo > 0) {
+			if (gameObject.GetComponentInParent<AmmoRemaining> ().ammo > 0 && gameObject.GetComponentInParent<AmmoRemaining>().type == EntityTypes.Teammate.Equals("Sergeant")) {
 				if (Input.GetButton ("Fire1") && Time.time >= nextTimeToFire) {
+					nextTimeToFire = Time.time + 2f / fireRate;
+					Shoot ();
+				}
+			}
+			else if (gameObject.GetComponentInParent<AmmoRemaining> ().ammo > 0 && gameObject.GetComponentInParent<AmmoRemaining>().type == EntityTypes.Teammate.Equals("Doctor")) {
+				if (Input.GetButtonDown ("Fire1") && Time.time >= nextTimeToFire) {
+					nextTimeToFire = Time.time + 2f / fireRate;
+					Shoot ();
+				}
+			}
+			else if (gameObject.GetComponentInParent<AmmoRemaining> ().ammo > 0 && gameObject.GetComponentInParent<AmmoRemaining>().type == EntityTypes.Teammate.Equals("Engineer")) {
+				if (Input.GetButtonDown ("Fire1") && Time.time >= nextTimeToFire) {
+					nextTimeToFire = Time.time + 2f / fireRate;
+					Shoot ();
+				}
+			}
+			else if (gameObject.GetComponentInParent<AmmoRemaining> ().ammo > 0 && gameObject.GetComponentInParent<AmmoRemaining>().type == EntityTypes.Teammate.Equals("Captain")) {
+				if (Input.GetButtonDown ("Fire1") && Time.time >= nextTimeToFire) {
 					nextTimeToFire = Time.time + 2f / fireRate;
 					Shoot ();
 				}
