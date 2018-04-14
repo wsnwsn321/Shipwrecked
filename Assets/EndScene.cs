074e741ba656287b94b1601a9f4ed02328bf80fe
adam sturgeon <sturgeon.68@osu.edu>
2018-04-14 17:40:04 -0400

    Ship Destroyed Scene

--- Assets/EndScene.cs
+++ Assets/EndScene.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 
 
-public class EndScene : MonoBehaviour {
 
-	// Use this for initialization
+public class EndScene : MonoBehaviour {
+	public string[] Story;
+	public AudioClip[] clips;
+
 	void Start () {
 		StartCoroutine("PlayVideo");
 	}
@@ -20,6 +23,37 @@ public class EndScene : MonoBehaviour {
 
 	private IEnumerator PlayVideo()
 	{
+
+		yield return new WaitForSeconds(1f);
+		GameObject.Find ("Main Camera").GetComponent<AudioSource>().Play();	
+		yield return new WaitForSeconds(1f);
+		//type out story text
+		for (int i = 0; i < 4; i++) {
+
+			int randomClip = Random.Range (0, clips.Length);
+			//set text to next section of story
+			GameObject.Find ("Text").GetComponent<TypeOutScript> ().reset = true;
+			GameObject.Find ("Text").GetComponent<TypeOutScript> ().FinalText = Story [i];
+			GameObject.Find ("Text").GetComponent<TypeOutScript> ().On = true;
+			gameObject.GetComponent<AudioSource> ().loop = true;
+			gameObject.GetComponent<AudioSource>().clip = clips [0];
+			//randomClip
+			gameObject.GetComponent<AudioSource>().Play ();
+
+			while (GameObject.Find ("Text").GetComponent<Text> ().text.Length != GameObject.Find ("Text").GetComponent<TypeOutScript> ().FinalText.Length) {
+				yield return new WaitForSeconds (.1f);
+
+			}
+			gameObject.GetComponent<AudioSource> ().loop = false;
+			yield return new WaitForSeconds(3f); // wait to let people read
+		}
+		//set text to nothing
+		GameObject.Find ("Text").GetComponent<TypeOutScript> ().reset = true;
+
+		GameObject.Find("Cube").GetComponent<VideoPlayer>().Prepare();
+		GameObject.Find ("Main Camera").GetComponent<AudioSource>().Pause();
+		GameObject.Find("Cube").GetComponent<VideoPlayer>().Play();
+
 		yield return new WaitForSeconds(30f);
 		SceneManager.LoadScene ("RegularCredits");
 	}
