85df0724daf779c24fae987370c830681f3a0537
Chris Crain <chrisac309@gmail.com>
2018-04-16 14:32:01 -0400

    New cameraref prefab; added ping viewer

---
+++ Assets/PingViewer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PingViewer : Photon.MonoBehaviour {
+
+	private Text pingText;
+	private int updateCounter = 0;
+	private int ping = 0;
+
+	void Start() {
+		pingText = gameObject.GetComponent<Text> ();
+		pingText.text = "Ping: " + ping.ToString()+ " ms";
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		updateCounter++;
+		if (updateCounter > 20) {
+			updateCounter = 0;
+			ping = PhotonNetwork.GetPing ();
+			if (ping < 70) {
+				pingText.color = Color.green;
+			} else if (ping < 120) {
+				pingText.color = Color.yellow;
+			} else {
+				if (ping > 999) {
+					ping = 999;
+				} 
+				pingText.color = Color.red;
+			}
+			pingText.text = "Ping: " + ping.ToString () + " ms";
+		}
+	}
+}
