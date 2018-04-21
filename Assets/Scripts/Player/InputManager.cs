8029fd8effa82ec03f315e826fddb010de9e7d34
Jackojack2134 <jackojack2134@gmail.com>
2018-04-21 18:42:13 -0400

    Added controls to the InputManager script for the menus.

--- Assets/Scripts/Player/InputManager.cs
+++ Assets/Scripts/Player/InputManager.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class InputManager
+public class InputManager : MonoBehaviour
 {
     #region Controls
+
+    #region Gameplay
 
     public static bool Ability1()
     {
@@ -114,42 +116,86 @@ public static class InputManager
         return Input.GetKeyDown(KeyCode.BackQuote) || BButton();
     }
 
+    #endregion Gameplay
+
+    #region Menu
+
+    public static bool MenuNavigateDown()
+    {
+        bool navDown = Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) || DPadVertical() < -0.2f || LeftJoystickVertical() < -0.2f;
+
+        return navDown;
+    }
+
+    public static bool MenuNavigateUp()
+    {
+        bool navUp = Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || DPadVertical() > 0.2f || LeftJoystickVertical() > 0.2f;
+
+        return navUp;
+    }
+
+    public static bool MenuSelect()
+    {
+        return Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Return) || AButton() || StartButton();
+    }
+
+    #endregion Menu
+
+    // Reset the in-use states when there is no input.
+    public static void NoInput()
+    {
+        leftJoysticHorizontalInUse = false;
+        leftJoysticVerticalInUse = false;
+        rightJoysticHorizontalInUse = false;
+        rightJoysticVerticalInUse = false;
+        dPadHorizontalInUse = false;
+        dPadVerticalInUse = false;
+        leftTriggerInUse = false;
+        rightTriggerInUse = false;
+    }
+
     #endregion Controls
 
     #region Inputs
 
     #region Controller
 
+    private static bool leftJoysticHorizontalInUse = false;
     private static float LeftJoystickHorizontal()
     {
         float result = Input.GetAxis("Left Joystick Horizontal");
         return Mathf.Clamp(result, -1f, 1f);
     }
 
+    private static bool leftJoysticVerticalInUse = false;
     private static float LeftJoystickVertical()
     {
         float result = Input.GetAxis("Left Joystick Vertical");
         return Mathf.Clamp(result, -1f, 1f);
     }
 
+    private static bool rightJoysticHorizontalInUse = false;
     private static float RightJoystickHorizontal()
     {
         float result = Input.GetAxis("Right Joystick Horizontal");
         return Mathf.Clamp(result, -1f, 1f);
     }
 
+    private static bool rightJoysticVerticalInUse = false;
     private static float RightJoystickVertical()
     {
         float result = Input.GetAxis("Right Joystick Vertical");
         return Mathf.Clamp(result, -1f, 1f);
     }
 
+    private static bool dPadHorizontalInUse = false;
     private static float DPadHorizontal()
     {
         float result = Input.GetAxis("DPad Horizontal");
         return Mathf.Clamp(result, -1f, 1f);
     }
 
+    private static bool dPadVerticalInUse = false;
     private static float DPadVertical()
     {
         float result = Input.GetAxis("DPad Vertical");
