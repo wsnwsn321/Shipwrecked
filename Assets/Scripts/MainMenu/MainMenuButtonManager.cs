9e8277c4a3529b07c6f34c7347ccd9bd868ebe21
Ryan Liu <liu.3892@osu.edu>
2018-04-20 23:06:02 -0400

    Add manual navigation for menu by keyboard

--- Assets/Scripts/MainMenu/MainMenuButtonManager.cs
+++ Assets/Scripts/MainMenu/MainMenuButtonManager.cs
@@ -12,6 +12,9 @@ public class MainMenuButtonManager : MonoBehaviour
     private Button button;
     private AudioSource audioSource;
 
+    private GameObject[] buttonArr = new GameObject[5];
+    private int selectionIndex;
+
     // Use this for initialization
     void Start()
     {
@@ -20,6 +23,13 @@ public class MainMenuButtonManager : MonoBehaviour
 
         button = GetComponent<Button>();
         button.onClick.AddListener(TaskOnClick);
+
+        buttonArr[0] = GameObject.Find("Single Player");
+        buttonArr[1] = GameObject.Find("Multiplayer");
+        buttonArr[2] = GameObject.Find("Controls");
+        buttonArr[3] = GameObject.Find("Credits");
+        buttonArr[4] = GameObject.Find("Quit");
+        selectionIndex = 0;
     }
 
     private void Update()
@@ -28,6 +38,23 @@ public class MainMenuButtonManager : MonoBehaviour
         {
             TaskOnClick();
         }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            selectionIndex--;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            selectionIndex++;
+        }
+        if (selectionIndex < 0)
+        {
+            selectionIndex = buttonArr.Length - 1;
+        }
+        else if (selectionIndex >= buttonArr.Length)
+        {
+            selectionIndex = 0;
+        }
+        EventSystem.current.SetSelectedGameObject(buttonArr[selectionIndex]);
     }
 
     public void TaskOnClick()
