09e00840b56760e47760af1ab0b493102464179b
Jackojack2134 <jackojack2134@gmail.com>
2018-04-21 20:30:29 -0400

    Enemy stats that can be changed.

---
+++ Assets/EnemyStats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats {
+
+    public static class Brain
+    {
+        public static float Damage = 20f;
+        public static float Health = 100f;
+        public static float Speed = 5f;
+    }
+
+    public static class Critter
+    {
+        public static float Damage = 10f;
+        public static float Health = 75f;
+        public static float Speed = 5f;
+    }
+}
